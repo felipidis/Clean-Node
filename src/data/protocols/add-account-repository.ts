1fba69afecf354205919cc1bc916535e461ab98b
Felipe Pimentel <felipekdra@gmail.com>
2023-04-28 16:55:18 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

---
+++ src/data/protocols/add-account-repository.ts
@@ -0,0 +1,6 @@
+import { type IAddAccountModel } from '../../domain/usecases/add-account'
+import { type IAccountModel } from '../../domain/models/account'
+
+export interface IAddAccountRepository {
+  add: (accountData: IAddAccountModel) => Promise<IAccountModel>
+}
--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,14 +1,17 @@
-import { type IAccountModel, type IAddAccount, type IAddAccountModel, type IEncrypter } from './db-add-account-protocols'
+import { type IAddAccountRepository, type IAccountModel, type IAddAccount, type IAddAccountModel, type IEncrypter } from './db-add-account-protocols'
 
 export class DbAddAccount implements IAddAccount {
   private readonly encrypter: IEncrypter
+  private readonly addAccountRepository: IAddAccountRepository
 
-  constructor (encrypter: IEncrypter) {
+  constructor (encrypter: IEncrypter, addAccountRepository: IAddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
-  async add (account: IAddAccountModel): Promise<IAccountModel> {
-    await this.encrypter.encrypt(account.password)
+  async add (accountData: IAddAccountModel): Promise<IAccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
     return await Promise.resolve({ id: 'test', name: 'test', email: 'test', password: 'test' })
   }
 }
